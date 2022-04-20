789e52509e0f499bafb424b05315a2b2c8137a1e
MEKARI <mekari@MEKARIs-MacBook-Pro.local>
2022-04-20 11:53:56 +0700

    add penjelasan puts vs print vs p

--- PutsPrint.rb
+++ PutsPrint.rb
@@ -1,3 +1,10 @@
+## This method is to print .
+# For printing bisa menggunakan double quote atau single quote untuk string.
+# Untuk integer or number bisa tanpa menggunakan quote.
+# puts and p is like println , dia akan lgs membuat line baru.
+# print tidak membuat line baru.
+# puts vs print vs p in ruby : https://flexiple.com/puts-vs-p-vs-print-ruby
+
 puts 'Hello World'
 puts "I'm alive and well!"
 
