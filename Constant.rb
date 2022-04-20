7013ea13150d1368e1869cff5e2b24d41a1073e4
okfriansyah-moh <okfriansyah@gmail.com>
2022-04-20 14:18:56 +0700

    add Constant

---
+++ Constant.rb
@@ -0,0 +1,20 @@
+age = 25
+p age
+
+age = 30
+p age
+
+## constant is a variable that static or does not change
+## constant in ruby are begin with the CAPITAL LETTER
+# in convention, constant use UPPERCASE
+NAME = "BORIS"
+PI = 3.14159
+
+p NAME
+p PI
+
+## String interpolation
+p "My name is #{NAME}"
+
+NAME = "Jeff" ## we still can assign the constant var, but it is not recommended. Because constant is constant.
+p NAME
