635d04dcdcefbe385df4242a02e4a45c6ddcca2d
okfriansyah-moh <okfriansyah@gmail.com>
2022-04-20 13:37:25 +0700

    add arithmetic in ruby

--- PutsPrint.rb
+++ PutsPrint.rb
@@ -11,9 +11,30 @@ puts "I'm alive and well!"
 puts 5
 puts 3.14
 
-puts 4+3
-print 4+3
-p 4+3
+puts 4 + 3
+print 4 + 3
+p 4 + 3
 
 puts '4' + '3'
 puts
+
+
+### Arithmetic in Ruby
+p 1 + 4
+p 10 - 6
+p 3 * 4
+p "School" + "Bus"
+p 10 / 5
+p 12 / 5 ## ruby divide integer to integer, dia akan selalu balikin integer. Karena ruby selalu balikin datanya sesuai dengan operation type nya.
+p 12.0 / 5 ## kalau mau ada floating pointnya harus seperti ini
+p 12.0 / 5.0
+
+## Exponential symbol
+p 2 ** 5 ## 2 ^ 5
+p 3 ** 3 ## 3 ^ 3
+# modulo
+p 5 % 2 ## it will give us the remainder
+p 6 % 2 ## no remainder
+p 14 % 4 ## remainder is 2
+p 0.5 / 2
+p -0.75 / 2
