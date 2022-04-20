15628e10719454ed1fbf73a78380ad0251c4daf8
okfriansyah-moh <okfriansyah@gmail.com>
2022-04-20 14:26:56 +0700

    add object method

---
+++ ObjectMethod.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+puts 'hello world'.length
+puts 'hello world'.upcase
+puts 'HELLO WORLD'.downcase
+
+foo = 'hello world'
+p foo.length
+p foo.upcase
+
+puts
+
+p 11.next # increment by 1
+p -1.next
+
+puts 'Hello world'
+puts 'Hello world'.inspect ## the output is the same like p. Basically the p command is the short for this one
